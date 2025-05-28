b10264aad93549116ad9aa30a9f8dadbf2ebf596
M. Marcelo S. Salgado <mmssalgado09@gmail.com>
2025-05-28 12:33:56 +0200

    feat(Crew):  :sparkles: add Shift complete enum

--- src/main/java/com/springfield/powerplant/crew/Shift.java
+++ src/main/java/com/springfield/powerplant/crew/Shift.java
@@ -1,4 +1,17 @@
 package com.springfield.powerplant.crew;
 
 public enum Shift {
+    MORNING("Mañana"),
+    AFTERNOON("Tarde"),
+    NIGHT("Noche");
+
+    private final String shift;
+
+    Shift(String shift) {
+        this.shift = shift;
+    }
+
+    public String getShift() {
+        return shift;
+    }
 }
