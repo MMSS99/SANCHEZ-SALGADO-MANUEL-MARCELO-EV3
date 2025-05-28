adb169d2fbc0acb38130932d5032764111353d84
M. Marcelo S. Salgado <mmssalgado09@gmail.com>
2025-05-28 12:29:33 +0200

    feat(Crew):  :sparkles: add Department enum

--- src/main/java/com/springfield/powerplant/crew/Department.java
+++ src/main/java/com/springfield/powerplant/crew/Department.java
@@ -1,4 +1,16 @@
 package com.springfield.powerplant.crew;
 
 public enum Department {
+    REACTOR_CONTROL("Control de Reactor"),
+    SECURITY("Seguridad"),
+    MAINTENANCE("Mantenimiento"),
+    ADMINISTRATION("Administración");
+
+    private final String name;
+
+    Department(String name) {
+        this.name = name;
+    }
+
+
 }
