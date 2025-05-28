a42b4c9502add03529bb5af689056f28e906dfef
M. Marcelo S. Salgado <mmssalgado09@gmail.com>
2025-05-28 12:54:16 +0200

    feat(Manager):  :feat: add createEmployee method that stores (or creates a temporal employee) an employee in the database

--- src/main/java/com/springfield/powerplant/management/EmployeeManagementService.java
+++ src/main/java/com/springfield/powerplant/management/EmployeeManagementService.java
@@ -1,4 +1,10 @@
 package com.springfield.powerplant.management;
 
+import com.springfield.powerplant.crew.Department;
+import com.springfield.powerplant.crew.Employee;
+import com.springfield.powerplant.crew.ExperienceLevel;
+import com.springfield.powerplant.crew.Shift;
+
 public interface EmployeeManagementService {
+    Employee createEmployee(String name, Integer ID, Department department, ExperienceLevel experienceLevel, Shift shift);
 }
--- src/main/java/com/springfield/powerplant/management/EmployeeManagementServiceImpl.java
+++ src/main/java/com/springfield/powerplant/management/EmployeeManagementServiceImpl.java
@@ -1,14 +1,24 @@
 package com.springfield.powerplant.management;
 
+import com.springfield.powerplant.crew.Department;
 import com.springfield.powerplant.crew.Employee;
+import com.springfield.powerplant.crew.ExperienceLevel;
+import com.springfield.powerplant.crew.Shift;
 
 import java.util.HashMap;
 
 public class EmployeeManagementServiceImpl implements EmployeeManagementService {
     private HashMap<Integer, Employee> employeeDatabase = new HashMap<Integer, Employee>();
 
-    public void createEmployee(){
-        Employee employee = new Employee();
+    public Employee createEmployee(String name, Integer ID, Department department, ExperienceLevel experienceLevel, Shift shift){
+        Employee employee = new Employee(ID, name, department, experienceLevel, shift);
+        if (!employeeDatabase.containsKey(ID)){
+            employeeDatabase.put(ID, employee);
+            System.out.println("Empleado " + name + " asignado al departamento " + department.getDepartmentName() + '.');
+        } else {
+            System.out.println("El empleado con " + ID + " ya existe, por lo que el nuevo empleado no se ha validado en la base de datos.");
+        }
+        return employee;
     }
 
 }
